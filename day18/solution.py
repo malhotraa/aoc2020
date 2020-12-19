5bdd94733518a614631dcf3e62a3505f69f717a1
Ashish Malhotra <ash6320@gmail.com>
2020-12-18 17:03:05 -0800

    Day18 attempt2

--- day18/solution.py
+++ day18/solution.py
@@ -1,63 +1,67 @@
-import re
+from functools import partial
+
 with open('input.txt') as f:
     lines = f.read().split('\n')
 
-def part1_evaluate_simple_expr(e):
-    expr = e.strip('()').split()
-    res = int(expr[0])
-    op = None
-    for i in range(1, len(expr)):
-        if expr[i] in ['+', '*']:
-            op = expr[i]
+def par1_precedence(op):
+    return 1
+
+def part2_precedence(op):
+    if op == '+':
+        return 1
+    elif op == '*':
+        return 0
+    else:
+        raise Exception("Unsupported op {}".format(op))
+
+def evaluate(precedence_fn, expr):
+    ops = []
+    values = []
+    i = 0
+    while i < len(expr):
+        if expr[i] == ' ':
+            pass
+        elif expr[i] == '(':
+            ops.append(expr[i])
+        elif expr[i].isdigit():
+            start = i
+            while i < len(expr) and expr[i].isdigit():
+                i +=1
+            values.append(int(expr[start:i]))
+            i -= 1
+        elif expr[i] == ')':
+            # Pop stacks and eval expression
+            while len(ops) > 0 and ops[-1] != '(':
+                op = ops.pop()
+                a = values.pop()
+                b = values.pop()
+                values.append(eval("{}{}{}".format(a, op, b)))
+            ops.pop()
+        elif expr[i] in ['*', '+']:
+            while len(ops) > 0 and ops[-1] != '(' and precedence_fn(ops[-1]) >= precedence_fn(expr[i]):
+                op = ops.pop()
+                a = values.pop()
+                b = values.pop()
+                values.append(eval("{}{}{}".format(a, op, b)))
+            ops.append(expr[i])
         else:
-            if op == '+':
-                res = res + int(expr[i])
-            elif op == '*':
-                res = res * int(expr[i])
-            else:
-                raise Exception('Op must be one of + or *')
-    return res
+            raise Exception("Unknown item {}".format(expr[i]))
+        i += 1
 
-def part1_evaluate(line):
-    done = False
-    while not done:
-        matches = re.findall(r'\([^\(\)]*\)', line)
-        if len(matches) == 0:
-            return part1_evaluate_simple_expr(line)
-        for match in matches:
-            line = line.replace(match, str(part1_evaluate_simple_expr(match)))
-        done = line.find('+') == -1 and line.find('*') == -1 and line.find('(') == -1 and line.find(')') == -1
-    return int(line)
-
-def part2_evaluate_simple_expr(e):
-    e = e.strip('()')
-    done = False
-    while not done:
-        matches = re.findall(r'(\d+ \+ \d+)', e)
-        if len(matches) == 0:
-            break
-        for match in matches:
-            a, op, b = match.split(' ')
-            e = e.replace(match, str(int(a) + int(b)))
-        done = e.find('+') == -1
-    return eval(e)
-
-def part2_evaluate(line):
-    done = False
-    while not done:
-        matches = re.findall(r'\([^\(\)]*\)', line)
-        if len(matches) == 0:
-            res = part2_evaluate_simple_expr(line)
-            return res
-        for match in matches:
-            line = line.replace(match, str(part2_evaluate_simple_expr(match)))
-        done = line.find('+') == -1 and line.find('*') == -1 and line.find('(') == -1 and line.find(')') == -1
+    while len(ops) > 0:
+        op = ops.pop()
+        a = values.pop()
+        b = values.pop()
+        values.append(eval("{}{}{}".format(a, op, b)))
+    
+    assert len(ops) == 0 and len(values) == 1
+    return values[0]
 
 def part1_homework_sum(lines):
-    return sum(list(map(part1_evaluate, lines)))
+    return sum(list(map(partial(evaluate, par1_precedence), lines)))
 
 def part2_homework_sum(lines):
-    return sum(list(map(part2_evaluate, lines)))
+    return sum(list(map(partial(evaluate, part2_precedence), lines)))
 
 print('part1 homework_sum: ', part1_homework_sum(lines))
 print('part2_homework_sum: ', part2_homework_sum(lines))
